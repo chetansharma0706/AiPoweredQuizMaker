aec486817c4bb95ff1e9acb9afb7f908e913baa2
chetansharma0706 <chetanbtechstudent@gmail.com>
2025-08-11 18:51:57 +0530

    add .env inside .gitignore

--- src/appwrite/client.ts
+++ src/appwrite/client.ts
@@ -4,9 +4,9 @@ export const appwriteConfig = {
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
   usersCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
   endpointUrl: import.meta.env.VITE_APPWRITE_ENDPOINT,
-  // apiKey: import.meta.env.VITE_APPWRITE_API_KEY,
-};
+  apiKey: import.meta.env.VITE_APPWRITE_API_KEY
 
+}
 const client = new Client()
   .setEndpoint(appwriteConfig.endpointUrl)
   .setProject(appwriteConfig.projectId);
